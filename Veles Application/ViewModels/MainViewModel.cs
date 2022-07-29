152947ef0c0ea4919eb0d276c0eacdbdcf4abd6c
Marcin Holcman <holcmanmarcin@gmail.com>
2022-07-29 15:08:30 +0200

    Add authorization for SignalR

--- Veles Application/ViewModels/MainViewModel.cs
+++ Veles Application/ViewModels/MainViewModel.cs
@@ -45,7 +45,7 @@ namespace Veles_Application.ViewModels
             ChangePanelCommand = new ViewModelCommand(ExecutePanelChange);
         }
 
-        private void ExecutePanelChange(object parameter)
+        private async void ExecutePanelChange(object parameter)
         {
             if (parameter == null)
                 return;
@@ -53,7 +53,12 @@ namespace Veles_Application.ViewModels
                 MidViewModel = new HomeViewModel();
             else if (parameter != null)
             {
-                MidViewModel = new ChatViewModel(parameter as Group);
+                ChatViewModel chatView = new ChatViewModel(parameter as Group);
+                chatView.OpenConnectionAsync();
+                MidViewModel = chatView;
+                //MidViewModel = new ChatViewModel(parameter as Group);
+                
+                
             }
             //else if (parameter.ToString() == "Options") needs implementation
 
